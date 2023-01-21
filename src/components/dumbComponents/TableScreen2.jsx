b71bfaeb16196afccec393e30b7b1e9b58b598cd
lucassoft <lmartinez@sooft.tech>
2023-01-21 11:14:18 -0300

    tabla nueva sin llamado a spinner

--- src/components/dumbComponents/TableScreen2.jsx
+++ src/components/dumbComponents/TableScreen2.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactScrollableFeed from 'react-scrollable-feed'
 import Footer from "./Footer";
-//import Spinner from "../spinner";
 import "./arrow.css"
 import "./table.css"
 
